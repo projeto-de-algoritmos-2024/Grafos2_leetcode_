b1d00bfd6a9a6a207997463437105a6a4dd2a439
Joel <98978800+JoelSRangel@users.noreply.github.com>
2024-12-01 22:12:50 -0300

    1928: adicionando resolução final
    Co-authored-by: Izarias <pedroaugustoizarias@gmail.com>

--- Questoes/1928.py
+++ Questoes/1928.py
@@ -1,59 +1,6 @@
-from heapq import heapify, heappop, heappush
+from heapq import heappop, heappush
 
-class Graph:
-    def __init__(self, graph: dict = {}, passingFees: list = []):
-      self.graph = graph  # A dictionary for the adjacency list
-      self.passingFees = passingFees  # Lista de passing fees para cada cidade
-
-    def add_edge(self, node1, node2, weight):
-      # Adiciona node1 ao grafo, se necessário
-      if node1 not in self.graph:
-        self.graph[node1] = {}
-      # Adiciona node2 ao grafo, mesmo que não tenha arestas saindo dele
-      if node2 not in self.graph:
-        self.graph[node2] = {}
-      # Adiciona a aresta de node1 para node2
-      self.graph[node1][node2] = weight
-
-    def add_edges_from_list(self, edges):
-      for edge in edges:
-        self.add_edge(edge[0], edge[1], edge[2])
-
-    def shortest_distances_by_fees(self, source: str):
-      # Inicializa as distâncias com infinito e o custo dos vértices com infinito
-      costs = {node: float("inf") for node in self.graph}
-      costs[source] = self.passingFees[source]  # O custo inicial é o custo do vértice de origem
-
-      # Inicializa os caminhos com listas vazias
-      paths = {node: [] for node in self.graph}
-      paths[source] = [source]
-
-      # Inicializa a fila de prioridade (min-heap) com o custo de origem
-      pq = [(self.passingFees[source], source)]  # (custo, nó)
-      heapify(pq)
-
-      # Criar um conjunto para manter os nós visitados
-      visited = set()
-
-      while pq:
-          current_cost, current_node = heappop(pq)
-
-          if current_node in visited:
-              continue  # Pula nós já visitados
-          visited.add(current_node)
-
-          for neighbor in self.graph[current_node].keys():
-              # Cálculo do custo total (acumulado de passing fees)
-              tentative_cost = current_cost + self.passingFees[neighbor]
-
-              if tentative_cost < costs[neighbor]:
-                  costs[neighbor] = tentative_cost
-                  paths[neighbor] = paths[current_node] + [neighbor]
-                  heappush(pq, (tentative_cost, neighbor))
-
-      return costs, paths
-
-class Solution(object):
+class Solution:
     def minCost(self, maxTime, edges, passingFees):
         """
         :type maxTime: int
@@ -61,4 +8,38 @@ class Solution(object):
         :type passingFees: List[int]
         :rtype: int
         """
-        #djikstra porém vai adicionar mais os valores dos vértices
+        # Número de cidades
+        n = len(passingFees)
+
+        # Criar o grafo como lista de adjacências
+        graph = {i: [] for i in range(n)}
+        for x, y, time in edges:
+            graph[x].append((y, time))
+            graph[y].append((x, time))
+
+        # Min-heap: (custo, tempo, cidade)
+        pq = [(passingFees[0], 0, 0)]  # Inicia na cidade 0 com custo e tempo 0
+
+        # Distância mínima para cada cidade com um determinado tempo
+        min_time = {i: float('inf') for i in range(n)}
+        min_time[0] = 0
+
+        while pq:
+            cost, time, city = heappop(pq)
+
+            # Se chegou ao destino e está dentro do tempo, retorna o custo
+            if city == n - 1 and time <= maxTime:
+                return cost
+
+            # Explorar vizinhos
+            for neighbor, travel_time in graph[city]:
+                new_time = time + travel_time
+                new_cost = cost + passingFees[neighbor]
+
+                # Se o tempo for menor que o máximo permitido e melhora a solução, continua
+                if new_time <= maxTime and new_time < min_time[neighbor]:
+                    min_time[neighbor] = new_time
+                    heappush(pq, (new_cost, new_time, neighbor))
+
+        # Se nenhum caminho válido foi encontrado, retorna -1
+        return -1
